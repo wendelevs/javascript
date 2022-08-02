02d4ab6bbbbf069ae09e9f566dbdbe5d1e79cd24
Wendel Evangelista <107012134+wendelevs@users.noreply.github.com>
2022-08-02 16:12:03 -0300

    atualização do ex031 - JavaScript

--- exercicios-algoritmos/ex031-desafio/script.js
+++ exercicios-algoritmos/ex031-desafio/script.js
@@ -1,11 +1,12 @@
 function verificar() {
     var txtUser = document.querySelector('input#txtUser');
     var res = document.querySelector('div#res');
-    if (txtUser = 'pedra') {
-        res.innerHTML = '<strong>Papel</strong>';
-    } else if (txtUser == 'papel') {
-        res.innerHTML = '<strong>Tesoura</strong>';
-    } else if(txtUser == 'tesoura') {
-        res.innerHTML = '<strong>Pedra</strong>';
+    var retorno = '';
+    if (txtUser == 'Pedra') {
+        window.alert('Papel');
+    } else if (txtUser == 'Papel') {
+        window.alert('Tesoura');
+    } else {
+        window.alert('Pedra');
     }
 }
